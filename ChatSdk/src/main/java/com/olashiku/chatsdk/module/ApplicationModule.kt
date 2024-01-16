308b0e66b05f770648a870497b3ade7d87b544a3
oluwayemisioguntayo <olashiku@gmail.com>
2024-01-16 16:53:41 +0100

    version 11

--- ChatSdk/src/main/java/com/olashiku/chatsdk/module/ApplicationModule.kt
+++ ChatSdk/src/main/java/com/olashiku/chatsdk/module/ApplicationModule.kt
@@ -5,7 +5,6 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class ApplicationModule: MultiDexApplication() {
-
     override fun onCreate(){
         super.onCreate()
         startKoin {
